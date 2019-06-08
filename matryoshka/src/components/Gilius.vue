e05dfcacaa1c75d5e1a35ab60e3ce1adfdb8b7b3
gonzaloramirez <gonzaloramirez@google.com>
2019-06-08 12:53:34 -0500

    Examples

---
+++ matryoshka/src/components/Gilius.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hero gilius">
+    <img src="@/assets/gilius.png" />
+    <div>
+      <h5>Life: {{life}}</h5>
+      <h5>Attack: {{attack}}</h5>
+    </div>
+    <button @click="$emit('attack-now', $event)">Attack</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gilius",
+  data: () => {
+    return {
+      attack: 20,
+      life: 100
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hero img {
+  height: 150px;
+} 
+</style>
---
+++ matryoshka/src/components/TyrisFlare.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hero tyrisflare">
+    <img src="@/assets/tyris.png" />
+    <div>
+      <h5>Life: {{life}}</h5>
+      <h5>Attack: {{attack}}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TyrisFlare",
+  data: () => {
+    return {
+      attack: 30,
+      life: 100
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hero img {
+  height: 150px;
+} 
+</style>
